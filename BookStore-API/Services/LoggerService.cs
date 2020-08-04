d1b2a019a8e608c9c4880e7ab608f110563ef220
Auke Keijser <auke.keijser@mintgroup.net>
2020-08-04 14:51:53 +0200

    Add project files.

---
+++ BookStore-API/Services/LoggerService.cs
@@ -0,0 +1,29 @@
+﻿using BookStore_API.Contracts;
+using NLog;
+
+namespace BookStore_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void logDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void logError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void logInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void logWarn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
